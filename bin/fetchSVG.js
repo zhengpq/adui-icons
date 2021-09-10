753cce712b439144a77d592f94201e87430b1f72
zhengpq <1455760759@qq.com>
2021-09-10 17:01:38 +0800

    Update fetchSVG.js
    feat: 文件路径更换

--- bin/fetchSVG.js
+++ bin/fetchSVG.js
@@ -6,7 +6,8 @@ const PQueue = require('p-queue')
 require('dotenv').config()
 // const { FIGMA_TOKEN, FIGMA_FILE_URL, ADUI_ID} = process.env
 const FIGMA_TOKEN = '137867-237b6948-8462-4510-be5b-bee09a85c7cc'
-const FIGMA_FILE_URL = 'https://www.figma.com/file/PFuQDWtYvTBqy4wSz7tzTB/AD-UI-%E8%AE%BE%E8%AE%A1%E8%AF%AD%E8%A8%80?node-id=40%3A5'
+// const FIGMA_FILE_URL = 'https://www.figma.com/file/PFuQDWtYvTBqy4wSz7tzTB/AD-UI-%E8%AE%BE%E8%AE%A1%E8%AF%AD%E8%A8%80?node-id=40%3A5'
+const FIGMA_FILE_URL = 'https://www.figma.com/file/Sf7hJMvx2zhLwfapHiXMLB/AD-UI-%E8%AE%BE%E8%AE%A1%E8%AF%AD%E8%A8%80?node-id=40%3A5'
 const ADUI_ID = '44:124'
 
 const options = {
